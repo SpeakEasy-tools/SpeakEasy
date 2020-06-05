aa95338fa5b0f263df3bab6925e848ae52ae7ec6
JustinGOrefice <JustinGOrefice@gmail.com>
2020-06-04 23:09:56 -0400

    cleaned up and added correct formatting to about page and 2048 instructions

--- src/Views/Games/2048/Instructions.js
+++ src/Views/Games/2048/Instructions.js
@@ -2,10 +2,16 @@ import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import {Theme} from "../../../utils";
 import clsx from "clsx";
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
     root: {
-    	whiteSpace: 'pre-line'
+        height: '100%',
+    },
+    instructions: {
+        whiteSpace: 'pre-line',
+        // width: '50%',
+        objectFit: 'scale-down',
     }
 }));
 
@@ -14,17 +20,18 @@ export default () => {
 
     return (
         <div className={clsx(classes.root)}>
-        <p>
-        The goal of 2048 is to create a tile with the value 2048, which is done by combining tiles that have
-        the same number. This can be done by pressing the arrow keys on your keyboard to slide all tiles
-        in the designated direction. When two tiles of the same number collide, they will merge together 
-        with the combined value of the two tiles. After every move, a new tile will randomly appear with a 
-        value of two or four. Tiles with different numbers cannot combine, and once the board is completely 
-        filled with tiles the game is over. If a move causes three consecutive tiles of the same value to 
-        slide together, only the two tiles farthest along the direction of motion will combine. If all 
-        four spaces in a row or column are filled with tiles of the same value, a move parallel to that 
-        row/column will combine the first two and last two. 
-        </p>
+            <div className={clsx(classes.instructions)}>
+                <Typography variant="body1">
+                The goal of 2048 is to create a tile with the value 2048 by combining tiles with the same 
+                number to add them together. This can be done by pressing the arrow keys on your keyboard 
+                to slide all tiles in the designated direction. After every move, a new 2 or 4 tile will 
+                randomly appear. Once the board is completely filled with tiles the game is over. {"\n\n"}
+                If a move causes three consecutive tiles of the same value to slide together, only the two 
+                tiles farthest along the direction of motion will combine. If all four spaces in a row or 
+                column are filled with tiles of the same value, a move parallel to that row/column will 
+                combine the first two and last two. 
+                </Typography>
+            </div>
         </div>
     );
 };

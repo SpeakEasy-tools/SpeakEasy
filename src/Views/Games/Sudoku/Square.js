3272ff7035e019aaa3d4dbc6009aee94db7025e2
Justin Orefice <39930031+JustinGOrefice@users.noreply.github.com>
2020-06-17 15:58:28 -0400

    Added error highlighting and initialize board function (#77)
    * added detail to About Us page, instructions for 2048, and settings for sudoku
    
    * cleaned up and added correct formatting to about page and 2048 instructions
    
    * A few typography changes for consistency.
    
    * added settings and instructions for sudoku, and created word search and added settings and instructions
    
    * fixing conflicts
    
    * fixing conflicts
    
    * fixing conflicts
    
    * Delete 2048.js
    
    * Update Settings.js
    
    * Update Tile.js
    
    * Update Routes.js
    
    * lint
    
    * finished lint
    
    * finished sudoku
    
    * added initial board function and error highlighting
    
    * Algorithmic changes to sudoku
    
    Co-authored-by: Jeramey <tylerj2@rpi.edu>

--- src/Views/Games/Sudoku/Square.js
+++ src/Views/Games/Sudoku/Square.js
@@ -1,86 +1,60 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Theme } from "../../../utils";
 import clsx from "clsx";
 import Tile from "./Tile";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
     root: {},
     row: {
         width: "100%",
         display: "flex"
+    },
+    pad: {
+        padding: theme.spacing(1),
+        border: `1px solid ${theme.palette.secondary.light}`,
+        flex: "1 1 auto",
+        width: "75px",
+        "&:hover": {
+            cursor: "pointer"
+        }
     }
 }));
 
 function Square({ vals, squareId, handleClick }) {
     const classes = useStyles(Theme);
 
-    const handleTileClick = (tileId, val) => {
-        return handleClick(squareId, tileId, val);
+    const handleTileClick = (tileId, e) => {
+        return handleClick(squareId, tileId, e);
     };
 
+    useEffect(() => {}, [vals]);
     return (
         <div className={clsx(classes.root)}>
-            <div className={clsx(classes.row)}>
-                <Tile
-                    onClick={val => handleTileClick(0, val)}
-                    val={vals[0]}
-                    id={squareId.toString() + "0"}
-                />
-                <Tile
-                    onClick={val => handleTileClick(1, val)}
-                    val={vals[1]}
-                    id={squareId.toString() + "1"}
-                />
-                <Tile
-                    onClick={val => handleTileClick(2, val)}
-                    val={vals[2]}
-                    id={squareId.toString() + "2"}
-                />
-            </div>
-            <div className={clsx(classes.row)}>
-                <Tile
-                    onClick={val => handleTileClick(3, val)}
-                    val={vals[3]}
-                    id={squareId.toString() + "3"}
-                />
-                <Tile
-                    onClick={val => handleTileClick(4, val)}
-                    val={vals[4]}
-                    id={squareId.toString() + "4"}
-                />
-                <Tile
-                    onClick={val => handleTileClick(5, val)}
-                    val={vals[5]}
-                    id={squareId.toString() + "5"}
-                />
-            </div>
-            <div className={clsx(classes.row)}>
-                <Tile
-                    onClick={val => handleTileClick(6, val)}
-                    val={vals[6]}
-                    id={squareId.toString() + "6"}
-                />
-                <Tile
-                    onClick={val => handleTileClick(7, val)}
-                    val={vals[7]}
-                    id={squareId.toString() + "7"}
-                />
-                <Tile
-                    onClick={val => handleTileClick(8, val)}
-                    val={vals[8]}
-                    id={squareId.toString() + "8"}
-                />
-            </div>
+            {vals.map((v, i) => (
+                <div key={`${squareId}_${i}`} className={clsx(classes.row)}>
+                    {v.map(t => (
+                        <div
+                            key={t.id}
+                            className={clsx(classes.pad)}
+                            onClick={e =>
+                                handleTileClick(t.id, e.currentTarget)
+                            }
+                        >
+                            <Tile {...t} />
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
     );
 }
 
 Square.displayName = "Square";
 Square.propTypes = {
-    vals: PropTypes.any,
-    squareId: PropTypes.any,
-    handleClick: PropTypes.any
+    vals: PropTypes.array.isRequired,
+    squareId: PropTypes.number.isRequired,
+    handleClick: PropTypes.func.isRequired
 };
 export default Square;

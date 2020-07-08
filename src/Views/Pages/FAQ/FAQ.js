5f17f9ddba0b822f893a10d7abb9092e4a9d2008
JustinGOrefice <JustinGOrefice@gmail.com>
2020-07-08 16:20:03 -0400

    one more update

--- src/Views/Pages/FAQ/FAQ.js
+++ src/Views/Pages/FAQ/FAQ.js
@@ -43,7 +43,7 @@ function FAQ() {
         "SpeakEasy.tools is a data-driven web application for helping people learn a second language through artificial intelligence, data science, immersion, and gamification.",
         "SpeakEasy.tools has a portable and unique combination of tone training, fun and educational games, and informative progress tracking allows you to learn to read a language faster and learn more accurate pronunciation than the average language learning program.",
         "To create a profile, click the profile icon to open up the sign in menu and select your preferred sign in method. Currently supported sign in methonds are email, Google, and GitHub.",
-        "To start learning, select one of the games or tools from the menu on the left. The tools include things like a dictionary, flashcards, tone trainer, and pop quizes, while the games include Sudoku, 2048, Kakuro, Eye Spy, 15-puzzle, Uno, and Simon.",
+        "To start learning, select one of the games or tools from the menu on the left. The tools include things like a dictionary, flashcards, tone trainer, and pop quizes, while the games include 2048, Eye Spy, Sudoku, Memory, and Tile Slider.",
         "No, but wihtout a profle your experience will not be tailored to you over time and you won't be able to keep track of your progress."
     ];
 
